031528c505fb9668514941e27d68031db6c6334a
Chrysoula Bastianou <sph2000113@uoa.gr>
2025-11-06 20:33:24 +0200

    Update finite_state_m_TB.vhd

--- sim_1/finite_state_m_TB.vhd
+++ sim_1/finite_state_m_TB.vhd
@@ -7,7 +7,7 @@ end finite_state_m_TB;
 
 architecture testbench of finite_state_m_TB is
 
-    -- ”ﬁÏ·Ù· „È· ÙÔ FSM
+
     signal CLK         : std_logic := '0';
     signal RESET       : std_logic := '0';
     signal op          : std_logic_vector(1 downto 0) := "00";
@@ -76,7 +76,7 @@ begin
         op <= "00"; NoWrite_in <= '0'; CondEx_in <= '1'; S <= '0'; Rd <= "0001";
         wait for CLK_PERIOD;
 
-        -- 2: ≈CMP
+        -- 2: √ÖCMP
         NoWrite_in <= '1';
         wait for CLK_PERIOD;
 
